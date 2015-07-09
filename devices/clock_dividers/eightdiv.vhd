52c00b651464d803e4376c4f6dad81df02dbecc1
Ian Graves <thegravian@gmail.com>
2015-07-09 16:23:53 -0500

    Removing boilerplate from the tools.

--- devices/clock_dividers/eightdiv.vhd
+++ devices/clock_dividers/eightdiv.vhd
@@ -2,14 +2,6 @@ library IEEE;
 library UNISIM;
 use IEEE.STD_LOGIC_1164.all;
 use UNISIM.vcomponents.all;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity eightdiv is
   port(
